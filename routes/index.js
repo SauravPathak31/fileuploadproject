92a5da4d4d2706c6069167625fc82ad5fd12015c
SauravPathak31 <60220335+SauravPathak31@users.noreply.github.com>
2020-05-15 13:48:17 +0530

    sd

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ const
 ;
 router.post('/delete', (req, res, next) => {
   console.log("inside delete")
-  res.send(req.data)
+  res.send(req.body)
   // const deleteBlob = async (containerName, blobName) => {
   //   return new Promise((resolve, reject) => {
   //       blobService.deleteBlobIfExists(containerName, blobName, err => {
